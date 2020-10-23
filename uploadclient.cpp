1a13718a4603f5f8ba4abce7148e1f7e0497ecac
hanleilei <llhan@artosyn.cn>
2020-10-23 19:36:46 +0800

    1. add upload client data process

--- uploadclient.cpp
+++ uploadclient.cpp
@@ -1,12 +1,16 @@
 #include "uploadclient.h"
 
+#include <QDateTime>
+#include <windows.h>
+#include <time.h>
+
 UploadClient::UploadClient(QObject *parent)
     : QTcpSocket(parent)
 {
     connect(this, SIGNAL(readyRead()), this, SLOT(onDataRecv()));
 }
 
-int UploadClient::uploadHourData(QString hourStr, double a18Value, double a19Value)
+qint64 UploadClient::uploadHourData(QString hourStr, double a18Value, double a19Value)
 {
     QString a18Flag = "N";
     QString a19Flag = "N";
@@ -14,10 +18,10 @@ int UploadClient::uploadHourData(QString hourStr, double a18Value, double a19Val
                                         "A18-Avg=%2,A18-flag=%3;A19-Avg=%4,A19-flag=%5&&")
             .arg(hourStr).arg(a18Value).arg(a18Flag).arg(a19Value).arg(a19Flag);
     qDebug()<<"hour data content" << uploadDataContent;
-    sendPacket(uploadDataContent);
+    return sendPacket(uploadDataContent);
 }
 
-int UploadClient::uploadMinData(QString minStr, double a18Value, double a19Value)
+qint64 UploadClient::uploadMinData(QString minStr, double a18Value, double a19Value)
 {
     QString a18Flag = "N";
     QString a19Flag = "N";
@@ -25,11 +29,11 @@ int UploadClient::uploadMinData(QString minStr, double a18Value, double a19Value
                                         "A18-Avg=%2,A18-flag=%3;A19-Avg=%4,A19-flag=%5&&")
             .arg(minStr).arg(a18Value).arg(a18Flag).arg(a19Value).arg(a19Flag);
     qDebug()<<"hour data content" << uploadDataContent;
-    sendPacket(uploadDataContent);
+    return sendPacket(uploadDataContent);
 }
 
 //##0097ST=22;CN=2061;PW=123456;MN=TJTYTYACFC0011;CP=&&DataTime=20160516130000;A18-Avg=40.28,A18-flag=N&&FE41
-int UploadClient::sendPacket(QString contentStr)
+qint64 UploadClient::sendPacket(QString contentStr)
 {
     QString crcCode = QString("%1").arg(getCRC(contentStr), 4, 16, QLatin1Char('0'));
     QString dataStr = QString("%1%2\r\n").arg(contentStr).arg(crcCode);
@@ -44,6 +48,29 @@ void UploadClient::onDataRecv()
     static QString strLeft = QString();
     QString dataRecv = readAll();
     qDebug()<<"dataRecv" << dataRecv.size() << dataRecv;
+
+    if(dataRecv.contains("\r\n"))
+    {
+        int endPos = dataRecv.indexOf("\r\n");
+        qDebug()<<"endPos";
+    }
+    else
+    {
+        strLeft.append(dataRecv);
+    }
+}
+
+bool UploadClient::setSystemDateTime(QDateTime dt)
+{
+    SYSTEMTIME systm;
+    systm.wYear = dt.date().year();
+    systm.wMonth = dt.date().month();
+    systm.wDay = dt.date().day();
+    systm.wHour = dt.time().hour();
+    systm.wMinute = dt.time().minute();
+    systm.wSecond = dt.time().second();
+
+    return SetSystemTime(&systm);
 }
 
 int  UploadClient::getCRC(QString dataStr)
@@ -52,7 +79,8 @@ int  UploadClient::getCRC(QString dataStr)
     int CRC = 0xFFFF;
     int Num = 0xA001;
     int inum = 0;
-    for (int j = 0; j < data212.size(); j++)
+
+    for (unsigned int j = 0; j < data212.size(); j++)
     {
         inum = data212[j];
         CRC = (CRC >> 8) & 0x00FF;
@@ -64,9 +92,7 @@ int  UploadClient::getCRC(QString dataStr)
             CRC = CRC >> 1;
 
             if (flag == 1)
-            {
                 CRC = CRC ^ Num;
-            }
         }
     }
     return CRC;
--- uploadclient.h
+++ uploadclient.h
@@ -10,15 +10,16 @@ class UploadClient : public QTcpSocket
     Q_OBJECT
 public:
     explicit UploadClient(QObject *parent = nullptr);
-    int uploadMinData(QString minStr, double a18Value, double a19Value);
-    int uploadHourData(QString HourStr, double a18Value, double a19Value);
+    qint64 uploadMinData(QString minStr, double a18Value, double a19Value);
+    qint64 uploadHourData(QString HourStr, double a18Value, double a19Value);
 
 private slots:
     void onDataRecv();
 
 private:
-    int  sendPacket(QString contentStr);
+    qint64  sendPacket(QString contentStr);
     int  getCRC(QString str);
+    bool setSystemDateTime(QDateTime dt);
 };
 
 #endif // UPLOADCLIENT_H
